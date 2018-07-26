d0e63081e85ca1b33b044f64d260d780d4cf5c05
LX1993728 <2652790899@qq.com>
2018-07-26 22:11:05 +0800

    add framework for springboot

---
+++ jms-activemq-springboot/src/main/java/com/liuxun/jms/JmsActivemqSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.liuxun.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsActivemqSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsActivemqSpringbootApplication.class, args);
+    }
+}
